c6ea2d36280e59f25e0b3b0dcd8c2fa0a93f6a73
Ryan Levick <ryan.levick@gmail.com>
2018-10-12 16:08:33 +0200

    Add clap

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,54 @@
 extern crate reqwest;
 extern crate tokio;
 extern crate futures;
+extern crate clap;
 
+use clap::{Arg, App};
 use tokio::prelude::*;
-use futures::Future;
-use futures::stream;
+use futures::{Future,stream};
 use reqwest::r#async::Client;
 
 fn main() {
-    tokio::run(run("http://localhost:3000/echo", 500, 10))
+    let matches =
+        App::new("Welle")
+        .version("0.1")
+        .author("Ryan Levick <ryan.levick@gmail.com>")
+        .about("Load testing for servers")
+        .arg(Arg::with_name("request_count")
+             .short("n")
+             .long("request-count")
+             .value_name("NUMBER")
+             .required(true)
+             .help("Total number of requests to make")
+             .takes_value(true))
+        .arg(Arg::with_name("concurrent-count")
+             .short("c")
+             .long("concurrent-count")
+             .value_name("NUMBER")
+             .help("Number of in flight requests allowed at a time.")
+             .takes_value(true))
+        .get_matches();
+
+    let request_count = matches.value_of("request_count").unwrap().parse::<usize>().expect("concurrent-count not a number");
+    let concurrent_count = matches.value_of("concurrent_count").unwrap_or("1").parse::<usize>().expect("concurrent-count not a number");
+
+    tokio::run(run("http://localhost:3000/echo", request_count, concurrent_count))
 }
 
-fn run(url: &'static str, n: usize, c: usize) -> impl Future<Item=(), Error=()> {
+fn run(url: &'static str, request_count: usize, concurrent_count: usize) -> impl Future<Item=(), Error=()> {
     let client = Client::new();
 
     let futures =
-        (0 .. n)
+        (0 .. request_count)
         .into_iter()
         .map(move |_| make_request(&client, url));
 
     stream::iter_ok(futures)
-        .buffer_unordered(c)
+        .buffer_unordered(concurrent_count)
         .collect()
-        .inspect(|rs| {
-            let sum: u32 = rs.iter().map(|d| d.subsec_millis()).sum();
-            println!("{}ms", sum as f64 / rs.len() as f64);
+        .inspect(move |rs| {
+            let sum: u32 = rs.iter().map(|d| d.subsec_micros()).sum();
+            println!("{}ms", (sum as f64 / request_count as f64) / 1000.0);
         })
         .map(|_| ())
 }
